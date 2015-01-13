23eb8af397e84d4a42c9e4a5c55435ff1fb885ab
claireellul <c.ellul@ucl.ac.uk>
2015-01-13 12:27:50 +0000

    db connect changes

--- ajax/dbconnect.php
+++ ajax/dbconnect.php
@@ -1,7 +1,8 @@
 <?php
-   $host        = "host=localhost";
-   $port        = "port=5432";
-   $dbname      = "dbname=threed";
-   $credentials = "user=postgres password=postgres";
+   $host        = "host=127.0.0.1";
+   $port        = "port=1432";
+   $dbname      = "dbname=user1db";
+   $credentials = "user=user1 password=user1password";
    $db = pg_connect( "$host $port $dbname $credentials"  );
+
 ?>
